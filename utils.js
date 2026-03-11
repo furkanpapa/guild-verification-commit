b3782beba963853e555e2a38a84f517a96ea0d80
furkanpapa <166498808+furkanpapa@users.noreply.github.com>
2026-03-11 14:49:49 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:49:43.227Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-11T11:49:50.529Z
+Commit: Fix memory leak in event handlers
