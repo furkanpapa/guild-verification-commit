8d99255afa91d8aa1e757a88221e739bc7a50461
furkanpapa <166498808+furkanpapa@users.noreply.github.com>
2026-03-11 14:50:05 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:49:50.529Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T11:50:06.618Z
+Commit: Fix CSS styling bug on mobile devices
