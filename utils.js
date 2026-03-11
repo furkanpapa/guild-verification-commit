050ba1ec8fa970acebc55488cdfb035ed88c2d13
furkanpapa <166498808+furkanpapa@users.noreply.github.com>
2026-03-11 14:51:47 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:50:06.618Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-11T11:51:48.509Z
+Commit: Resolve null pointer exception in user service
