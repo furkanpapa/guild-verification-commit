b600c2d61aacf543b66fec64c91cae6b4a003f61
furkanpapa <166498808+furkanpapa@users.noreply.github.com>
2026-03-11 13:57:07 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:56:51.182Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T10:57:07.866Z
+Commit: Fix memory leak in event handlers
