5b8e0c51e2dd7bae96f18c65a44bb853f1853282
furkanpapa <166498808+furkanpapa@users.noreply.github.com>
2026-03-11 14:49:44 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T10:57:06.732Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T11:49:44.478Z
+Commit: Resolve timezone issues in date handling
